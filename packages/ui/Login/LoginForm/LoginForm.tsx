9122e39b2c1a2172020257fe215e903b6c0ef6d4
DESKTOP-JNGH4DF\Eric <ericz2016@gmail.com>
2026-07-28 16:07:54 -0400

    feat: added fetch call to localhost to check inputed information

--- packages/ui/Login/LoginForm/LoginForm.tsx
+++ packages/ui/Login/LoginForm/LoginForm.tsx
@@ -11,6 +11,7 @@ export default function LoginForm() {
   const router = useRouter();
 
   const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
   const [form, setForm] = useState({
     username: "",
     password: "",
@@ -23,8 +24,44 @@ export default function LoginForm() {
     });
   };
 
-  const submitForm = () => {
-    //...
+  const submitForm = async (e) => {
+    e.preventDefault();
+
+    try {
+      setError("");
+      const response = await fetch("http://localhost:8080/api/Auth/Login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userName: form.username,
+          password: form.password,
+        }),
+      });
+
+      if (!response.ok) {
+        //check if the username and passowrd field are filled before changing the error
+        if (form.username === "" || form.password === "") {
+          setError("Please fill in the blank field!");
+        } else {
+          const errorData = await response.json().catch(() => null);
+          console.error("Login failed:", response.status, errorData);
+          setError("Incorrect username or password!");
+        }
+        return;
+      }
+
+      // Store the token so future requests can use it
+      const data = await response.json();
+      localStorage.setItem("token", data.accessToken);
+
+      //Push back to "main page"
+      router.push("/");
+    } catch (error) {
+      console.error("Network or unexpected error:", error);
+      setError("Network or unexpected error!");
+    }
   };
 
   const togglePasswordVisibility = () => {
@@ -32,7 +69,7 @@ export default function LoginForm() {
   };
 
   return (
-    <form className={styles.formOutline}>
+    <form className={styles.formOutline} onSubmit={submitForm}>
       <h4>Login</h4>
 
       {/* Username field */}
@@ -80,7 +117,7 @@ export default function LoginForm() {
       </div>
       <div className={styles.divSpliting}>
         <div className={styles.loginDiv}>
-          <button onClick={submitForm} className={styles.formSubmissionBtn}>
+          <button className={styles.formSubmissionBtn} type="submit">
             <p>Login</p>
           </button>
         </div>
@@ -91,10 +128,13 @@ export default function LoginForm() {
           </Link>
         </div>
       </div>
+
       <Link href={"/"} className={styles.backDiv}>
         <Image src={"/svg/red_back.svg"} width={24} height={24} alt="Arrow" />
         <p>Back</p>
       </Link>
+
+      <p className={styles.errorMsg}>{error}</p>
     </form>
   );
 }
