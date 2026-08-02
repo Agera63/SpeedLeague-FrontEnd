04f46b1afe68c3f7b67e200d281b68ff65209370
DESKTOP-JNGH4DF\Eric <ericz2016@gmail.com>
2026-08-02 11:09:24 -0400

    fix: fixed the url to online API

--- packages/api/setup.ts
+++ packages/api/setup.ts
@@ -1,6 +1,6 @@
 import { OpenAPI } from "./core/OpenAPI";
 
-OpenAPI.BASE = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8080";
+OpenAPI.BASE = process.env.NEXT_PUBLIC_API_URL ?? "https://sl-api-mu.vercel.app";
 OpenAPI.TOKEN = async () => {
   if (typeof window === "undefined") return "";
   return localStorage.getItem("token") ?? "";
